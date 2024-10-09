8d8f93e9c885e6ee456ebd3d93b519ae9c845f1a
Mohit Sharma <sharmamohit85627@gmail.com>
2024-10-09 17:27:39 +0530

    third commit

--- steps/ingest_data.py
+++ steps/ingest_data.py
@@ -11,7 +11,7 @@ class IngestData:
         return pd.read_csv(self.data_path)
     
 @step
-def ingest_data(data_path : str) -> pd.DataFrame:
+def ingest_df(data_path : str) -> pd.DataFrame:
     try:
         ingest_data = IngestData(data_path)
         return ingest_data.get_data()
